c7bee01062d7314bed4c21be0d9b34b661cc1f88
Maxi <maxi.barmetler@gmail.com>
2021-10-30 00:43:35 +0200

    shit

--- src/routes/alexa.ts
+++ src/routes/alexa.ts
@@ -16,28 +16,43 @@ alexaRouter.get('/state', async (req: Request<{}, any, {}, { id: string }>, res)
     if (!user.traffic_light.visible) {
         res.send({
             success: false,
-            message: "I don't see a traffic light."
+            message: "I don't see a traffic light.",
         })
         return
     }
     if (user.traffic_light.multiple) {
-        res.send('I see multiple traffic lights.')
+        res.send({
+            success: false,
+            message: 'I see multiple traffic lights.',
+        })
         return
     }
     if (user.traffic_light.non_pedestrian) {
-        res.send("I don't see a pedestrian traffic light.")
+        res.send({
+            success: false,
+            message: "I don't see a pedestrian traffic light.",
+        })
         return
     }
     if (user.traffic_light.red === undefined) {
-        res.send("I can't make out the color of the traffic light.")
+        res.send({
+            success: false,
+            message: "I can't make out the color of the traffic light.",
+        })
         return
     }
     if (user.traffic_light.red === true) {
-        res.send('The light is red, stay put and do not cross the street.')
+        res.send({
+            success: false,
+            message: 'The light is red, stay put and do not cross the street.',
+        })
         return
     }
     if (user.traffic_light.red === false) {
-        res.send('The light is green, you can pass the crossing,')
+        res.send({
+            success: true,
+            message: 'The light is green, you can pass the crossing,',
+        })
         return
     }
 })
