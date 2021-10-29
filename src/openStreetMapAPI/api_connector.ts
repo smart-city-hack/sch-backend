67a997878b4daab7c67e75b548f981e114808a76
Maria Pospelova <mariyapospelova@gmail.com>
2021-10-29 21:36:32 +0200

    Add connection to OpenStreetMap

---
+++ src/openStreetMapAPI/api_connector.ts
@@ -0,0 +1,27 @@
+import { getBoundingBoxCoordinates}  from "./coordinates_parser";
+import axios from "axios";
+
+function prepareData(coordinates: string) {
+    let bounding_box = `(${getBoundingBoxCoordinates(coordinates)})`;
+    return bounding_box;
+}
+
+const serverApi = axios.create({
+  baseURL: "https://www.overpass-api.de",
+  timeout: 90000,
+});
+
+export const getOpenMapData = (coordinates: string) => {
+  serverApi
+    .get("/api/interpreter", {
+      params: {
+        data: `[out:json];node[\"traffic_signals:sound\"=\"yes\"]${prepareData(coordinates)};out body;`
+      }
+    })
+    .then((res) => {
+      console.log("REQUEST SUCCESS", res);
+      console.log(res.data);
+    });
+};
+
+getOpenMapData("52:31:19.1712N 13:24:47.6784E");
